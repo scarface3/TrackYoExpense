74855065641331d10ff94cf1591a8afebb62cc05
scarface3 <49342290+scarface3@users.noreply.github.com>
2021-06-08 22:19:19 +0100

    Update ExpenseItem.js

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -4,8 +4,7 @@ import Card from "../UI/Card";
 import ExpenseDate from "./ExpenseDate";
 
 const ExpenseItem = (props) => {
-  const clickHnadler = () => {};
-
+ 
   return (
     <li>
       <Card className="expense-item">
@@ -14,7 +13,7 @@ const ExpenseItem = (props) => {
           <h2>{props.title}</h2>
           <div className=" expense-item__price">${props.amount}</div>
         </div>
-        <button onClick={clickHnadler}>Change Title</button>
+       
       </Card>
     </li>
   );
